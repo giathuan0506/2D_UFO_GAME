8802251c25ef8bf0218ed48a4f807eb51faefe7c
giathuan0506 <giathuandang16031999@gmail.com>
2020-07-17 14:18:57 +0700

    add new project

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public Vector3 offset;
+    public GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+        offset = this.transform.position - player.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Rigidbody2D r2;
+    public Vector2 direction;
+    public float speed = 3f;
+
+    public int score = 0;
+    // Use this for initialization
+    void Start () {
+        r2 = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        direction.x = h;
+        direction.y = v;
+
+        r2.AddForce(direction*speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Coins"))
+        {
+            score += 1;
+            col.gameObject.SetActive(false);
+        }
+    }
+}
