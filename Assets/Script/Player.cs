f9e564ce79a1fbb659ca249b2b661abec3f00e23
giathuan0506 <giathuandang16031999@gmail.com>
2020-07-17 16:36:24 +0700

    update

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
     public Rigidbody2D r2;
     public Vector2 direction;
-    public float speed = 3f;
+    public float speed = 100f;
 
     public int score = 0;
+
+    public Text scoretext;
+    public Text wintext;
+
     // Use this for initialization
     void Start () {
+        setscoreText();
         r2 = GetComponent<Rigidbody2D>();
+        wintext.text = "";
 	}
 	
 	// Update is called once per frame
@@ -21,7 +28,10 @@ public class Player : MonoBehaviour {
         direction.x = h;
         direction.y = v;
 
-        r2.AddForce(direction*speed);
+        r2.AddForce((direction.normalized*speed)*Time.deltaTime/0.5f);
+
+        if (score >= 6)
+            wintext.text = "YOU WIN !!";
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -30,6 +40,12 @@ public class Player : MonoBehaviour {
         {
             score += 1;
             col.gameObject.SetActive(false);
+            setscoreText();
         }
+    }
+
+    void setscoreText()
+    {
+        scoretext.text = "Score: " + score.ToString();
     }
 }
